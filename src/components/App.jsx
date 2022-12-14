aba57abdcc644a099db0dcb81c0668b3ec25ffb4
Oleksandr Bron <lexlutermotion@gmail.com>
2022-12-14 23:14:28 +0200

    profile section added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
+import data from '../data/data';
+import friends from '../data/friends';
+import transactions from '../data/transactions';
+import user from '../data/user';
+import { Section } from './Section/Section.styled';
+import { Heading } from './Heading/Heading.styled';
+import { Container } from './Container/Container.styled';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'styles/theme';
+import { Profile } from './Profile/Profile';
+
 export const App = () => {
   return (
-    <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101'
-    // }}
-    ></div>
+    <ThemeProvider theme={theme}>
+      <Section>
+        <Container>
+          <Heading marginBottom="50px" textAlign="center">
+            Block 1
+          </Heading>
+          <Profile user={user} />
+        </Container>
+      </Section>
+    </ThemeProvider>
   );
 };
---
+++ src/components/Profile/Profile.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: ${({ theme }) => theme.spacing(100)};
+  overflow: hidden;
+  margin-left: auto;
+  margin-right: auto;
+
+  box-shadow: ${({ theme }) => theme.shadows.regular};
+  border-radius: ${({ theme }) => theme.spacing(4)};
+  background: ${({ theme }) => theme.colors.white};
+`;
+
+export const Description = styled.div`
+  background-color: ${({ theme }) => theme.colors.grey};
+`;
+
+export const Avatar = styled.img`
+  border-radius: 50%;
+`;
