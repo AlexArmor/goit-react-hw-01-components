85c73e1971f20e25a5b4f0817bfea04fad61f8f8
Oleksandr Bron <lexlutermotion@gmail.com>
2022-12-16 03:00:03 +0200

    final fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import data from '../data/data';
 import friends from '../data/friends';
 import transactions from '../data/transactions';
 import user from '../data/user';
-import { Heading } from './Heading/Heading.styled';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/theme';
 import { Profile } from './Profile/Profile';
@@ -14,13 +13,9 @@ import { GlobalStyleComponent } from '../styles/GlobalStyles';
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Heading marginBottom="50px" textAlign="center"></Heading>
       <Profile user={user} />
-      <Heading marginBottom="50px" textAlign="center"></Heading>
       <Statistics title="Upload stats" stats={data} />
-      <Heading marginBottom="50px" textAlign="center"></Heading>
       <FriendList friends={friends} />
-      <Heading marginBottom="50px" textAlign="center"></Heading>
       <TransactionHistory items={transactions} />
       <GlobalStyleComponent />
     </ThemeProvider>
